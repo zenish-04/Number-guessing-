c48fb8d796be3bef1b52bc8f7dd4450ae513d9d3
Jenish Basnet <jenishbasnet@users.noreply.github.com>
2026-04-18 22:44:41 +0545

    new commit

--- Project1.py
+++ Project1.py
@@ -3,9 +3,10 @@ import random
 expected=random.randint(1,100)
 print("Lets start the game!!")
 while True:
-    quit=input("Do you want to quit the game.(Y/N):")
-    if(quit=="Y" or quit=="y"):
-     break
+ if(quit=="Y" or quit=="y"):
+    print("You have quit the game")
+    break
+ while True:
     guess=int(input("Enter your guess between 1 to 100:"))
     if guess<expected:
         print("Your guess is low")
